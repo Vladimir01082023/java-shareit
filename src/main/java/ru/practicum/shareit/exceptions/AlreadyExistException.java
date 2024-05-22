bae8998604b705a48917d506de5427af42309f2e
Vladimir01082023 <vladimir-malyshev-2014@mai.ru>
2024-05-22 15:34:36 +0300

    Add-contrs

--- src/main/java/ru/practicum/shareit/exceptions/AlreadyExistException.java
+++ src/main/java/ru/practicum/shareit/exceptions/AlreadyExistException.java
@@ -1,5 +1,7 @@
 package ru.practicum.shareit.exceptions;
 
-public class AlreadyExistException extends RuntimeException{
-    public AlreadyExistException(String message) {super(message);}
+public class AlreadyExistException extends RuntimeException {
+    public AlreadyExistException(String message) {
+        super(message);
+    }
 }
