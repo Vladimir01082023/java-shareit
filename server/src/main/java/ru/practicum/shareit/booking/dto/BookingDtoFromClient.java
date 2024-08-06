a03575d7529c0ee73c81911d7dff7c1916ba832d
Vladimir01082023 <vladimir-malyshev-2014@mai.ru>
2024-08-06 14:36:49 +0300

    Add-docker branch. Fixing errors

--- server/src/main/java/ru/practicum/shareit/booking/dto/BookingDtoFromClient.java
+++ server/src/main/java/ru/practicum/shareit/booking/dto/BookingDtoFromClient.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class BookingDTO {
+public class BookingDtoFromClient {
     private Long id;
     private Long itemId;
     private Long bookerId;
